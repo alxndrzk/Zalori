432dd9475f01ac73108c2070af9244cb18d857db
Alexander <aleand1605@gmail.com>
2023-04-04 15:25:08 +0700

    ko aku wes garap

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,25 +1,26 @@
 @extends('layouts.app')
 
-@section('title', 'Home Page | Shop Beta')
+@section('title', 'Home Page | Shop Betaa')
 
 @section('content')
     <div class="row">
-        @for ($i = 1; $i<5; $i++)
-        <div class="col-3">
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title {{ $i }}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    @if ($i%2 == 0)
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    @else
-                        <a href="#" class="btn btn-danger">Go somewhere</a>
-                    @endif
-                    
+        @for ($i = 1; $i < 5; $i++)
+            <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title {{ $i }}</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                        @if ($i % 2 == 0)
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        @else
+                            <a href="#" class="btn btn-danger">Go somewhere</a>
+                        @endif
+
+                    </div>
                 </div>
             </div>
-        </div>
         @endfor
     </div>
 @endsection
